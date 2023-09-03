da78a6a34bae4e773d1a7cb9986afba9e6b818d7
Dmitriy Kagarlickij <dmitriy@kagarlickij.com>
2023-09-03 20:38:49 +0200

    cleanup

--- main.tf
+++ main.tf
@@ -4,8 +4,6 @@ module "s3_bucket" {
 }
 
 module "iam_policy" {
-  /* source  = "./modules/iam_policy" */
-  /* source = "github.com/kagarlickij/tf-pipeline-iam_policy" */
   source = "git::https://github.com/kagarlickij/tf-pipeline-iam_policy.git"
   s3_name = var.s3_name
 }
